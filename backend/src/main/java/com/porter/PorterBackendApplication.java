28f4a5af5814b531de7d5231072cf6cad465ae36
Dominic Chen <cchendominic@gmail.com>
2026-07-31 20:01:40 -0400

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/com/porter/PorterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.porter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorterBackendApplication.class, args);
+	}
+
+}
